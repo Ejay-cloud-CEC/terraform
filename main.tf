a0ab26c3b0977625ea3a3cd5c7cb2ae13100af52
Ejay-cloud-CEC <ejaycloudcec@gmail.com>
2026-06-05 15:00:12 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,19 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
 module "my_vpc" {
-  source = "./modules/vpc"
+  source   = "./modules/vpc"
   vpc_cidr = "10.0.0.0/16"
-  env = "cebu"
+  env      = "cebu"
 }
